cd0798e97571fa76180abbc8fbebf00f4200e77c
Kidist Dinku <95876178+Ney2@users.noreply.github.com>
2022-10-03 01:22:12 +0300

    Add solution for insertion sort part 1

---
+++ InsertionSortPart1.js
@@ -0,0 +1,18 @@
+function insertionSort1(n, arr) {
+    const sorted = [...arr].sort((a, b) => a - b);
+   let value = arr[arr.length - 1]
+   for(let i =1; i<=arr.length; i++) {
+    if(arr[arr.length -i-1] > value) {
+      arr[arr.length - i] = arr[arr.length -i -1]
+      console.log(arr.join(" "))
+    } else {
+      let temp = arr[arr.length-i-1]
+      arr[arr.length - i] = value;
+      value = temp
+     console.log(arr.join(" "))
+    }
+         if (JSON.stringify(arr) === JSON.stringify(sorted)) {
+           break
+         }
+     } 
+}
