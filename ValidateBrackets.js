7a18ae81178c47e89e7d983c89a64f3178940dc7
Kidist Dinku <95876178+Ney2@users.noreply.github.com>
2022-10-04 23:22:31 +0300

    Add solution for validating bracket

---
+++ ValidateBrackets.js
@@ -0,0 +1,22 @@
+var isValidate = function(s) {
+    var array = [];
+    var map = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    }
+    for (var i = 0; i < s.length; i++) {
+        if (s[i] === '(' || s[i] === '[' || s[i] === '{') {
+            array.push(s[i]);
+        } else {
+            var last = array.pop();
+            if (s[i] !== map[last]) {
+                return false;
+            }
+        }
+    }
+    if (array.length !== 0) {
+        return false;
+    }
+    return true;
+}
