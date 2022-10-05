2867330560c6c2d84fbfaeb7e19523eeb85661f2
Kidist Dinku <95876178+Ney2@users.noreply.github.com>
2022-10-05 11:28:05 +0300

    Add solution for closest point

---
+++ ClosestPoint.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[][]} points
+ * @param {number} k
+ * @return {number[][]}
+ */
+ var kClosest = function(points, k) {
+    let dist = [];
+    for (let i = 0; i < points.length; i++) {
+        dist.push([Math.sqrt(points[i][0] ** 2 + points[i][1] ** 2), i]);
+    }
+    dist.sort((a, b) => a[0] - b[0]);
+    let res = [];
+    for (let i = 0; i < k; i++) {
+        res.push(points[dist[i][1]]);
+    }
+    return res;
+};
