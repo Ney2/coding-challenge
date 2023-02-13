29b3354e34fec4c2e3c2862b7f946b4014f7e286
Kidist D. Guta <kidistdinku5057@gmail.com>
2023-02-13 16:55:56 +0300

    Time: 69 ms (73.93%), Space: 41.8 MB (62.14%) - LeetHub

---
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var countOdds = function(low, high) {
+     if (low % 2 == 0)
+        low++;
+    if (high % 2 == 0)
+        high--;
+    return (high - low) / 2 + 1;
+};
